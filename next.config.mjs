dd84baffc5149054facb5efbcd083aca38626ff7
realkoder <alex8970@stud.kea.dk>
2024-01-25 21:31:19 +0100

    Modified again again next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
-  
-    /**
-     * Set base path. This is usually the slug of your repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/reversed-guess",
-  
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+export default nextConfig;
